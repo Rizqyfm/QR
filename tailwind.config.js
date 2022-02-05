e8aae5ad9a0899004534a178acf6998b97796588
Rizqy Fachri <rizqyfachri@gmail.com>
2022-02-05 14:12:25 +0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'outfit' : ['Outfit'],
+      },
+      colors: {
+        'white' : 'hsl(0, 0%, 100%)',
+        'LG': 'hsl(212, 45%, 89%)',
+        'GB': 'hsl(220, 15%, 55%)',
+        'DB': 'hsl(218, 44%, 22%)',
+        'blu': 'hsl(228, 45%, 44%)',
+      },
+    },
+  },
+  plugins: [],
+}
